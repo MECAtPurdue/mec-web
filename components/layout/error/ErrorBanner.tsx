ad465118f8db64e118ba06d43f3bbd48b9fefde2
Daniyal Bekinalkar <daniyal.bekinalkar@gmail.com>
2024-10-26 15:08:00 -0500

    Update styling

--- components/layout/error/ErrorBanner.tsx
+++ components/layout/error/ErrorBanner.tsx
@@ -7,24 +7,24 @@ import { Cross2Icon } from "@radix-ui/react-icons";
 const ErrorBanner = () => {
   const { message, count, isError, clear } = useErrorStore();
 
-  if (!isError) return null;
+    if (!isError) return null;
 
-  return (
-    <div className="fixed top-16 left-1/2 transform -translate-x-1/2 bg-red-500 text-white px-6 py-4 z-50 w-full">
-      {/* <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 h-[72px]"></div> */}
-      <div className="flex justify-between items-center">
-        <div>
-          {message} <span>({count})</span>
+    return (
+      <div className="fixed top-[72px] z-50 w-full">
+        {/* <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 h-[72px]"></div> */}
+        <div className="bg-red-500 flex justify-between items-center container mx-auto max-w-screen-xl p-4 rounded-lg">
+          <div>
+            {message} <span>({count})</span>
+          </div>
+          <button
+            onClick={clear}
+            className="text-white hover:text-gray-300 focus:outline-none text-lg"
+          >
+            <Cross2Icon />
+          </button>
         </div>
-        <button
-          onClick={clear}
-          className="text-white hover:text-gray-300 focus:outline-none text-lg"
-        >
-          <Cross2Icon />
-        </button>
       </div>
-    </div>
-  );
+    );
 };
 
 export default ErrorBanner;
